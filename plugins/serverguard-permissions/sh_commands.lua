36775df12b69a3809ff4a174f4e0c0d96452642d
Alexander <48864677+Dobytchick@users.noreply.github.com>
2023-02-12 01:01:27 +0600

    Update sh_commands.lua

--- plugins/serverguard-permissions/sh_commands.lua
+++ plugins/serverguard-permissions/sh_commands.lua
@@ -1,18 +1,13 @@
 local PLUGIN = PLUGIN or {}
 
-function PLUGIN:InitializedPlugins()
-    for k, v in pairs(PLUGIN.PERMISSIONS) do
-        local fetchCommand = nut.command.list[k:lower()]
-        if not fetchCommand then continue end
-        local oldAccessFunc = fetchCommand.onCheckAccess
+for k, v in pairs(PLUGIN.PERMISSIONS) do
+    local fetchCommand = nut.command.list[k:lower()]
+    if not fetchCommand then continue end
+    local oldAccessFunc = fetchCommand.onCheckAccess
 
-        if oldAccessFunc then
-            fetchCommand.onCheckAccess = function(ply)
-                if not serverguard.player:HasPermission(ply, v) then return end
-                oldAccessFunc(ply)
-            end
-        end
-
-        nut.command.add(k:lower(), fetchCommand)
+    if oldAccessFunc then
+        fetchCommand.onCheckAccess = function(ply) return serverguard.player:HasPermission(ply, v) and oldAccessFunc(ply) end
     end
+
+    nut.command.add(k:lower(), fetchCommand)
 end
